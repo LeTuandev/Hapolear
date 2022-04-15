311b6829ac5342cbb9ab16747385128f6d2836db
LeTuandev <tuanlehuu1608@gmail.com>
2022-04-15 09:22:42 +0700

    config space in file auth.js

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
-    if($('.form-control').hasClass('form-log')) {
-        $('#loginModal').modal();
-        $('#closeRegisterForm').css('display', 'none');
-        $('#btnLogin').addClass('link-login');
-        $('#btnRegister').removeClass('link-register');
-    }
+$(document).ready(function () {
+  if ($('.form-control').hasClass('form-log')) {
+    $('#loginModal').modal();
+    $('#closeRegisterForm').css('display', 'none');
+    $('#btnLogin').addClass('link-login');
+    $('#btnRegister').removeClass('link-register');
+  }
 
-    if($('.form-control').hasClass('form-reg')) {
-        $('#loginModal').modal();
-        $('#closeFormLogin').css('display', 'none');
-        $('#btnLogin').removeClass('link-login')
-        $('#btnRegister').addClass('link-register')
-    }
+  if ($('.form-control').hasClass('form-reg')) {
+    $('#loginModal').modal();
+    $('#closeFormLogin').css('display', 'none');
+    $('#btnLogin').removeClass('link-login')
+    $('#btnRegister').addClass('link-register')
+  }
 
-    if($('#message').hasClass('alert-success')) {
-        $('#loginModal').modal();
-        $('#closeRegisterForm').css('display', 'none');
-        $('#btnLogin').addClass('link-login');
-        $('#btnRegister').removeClass('link-register');
-    }
+  if ($('#message').hasClass('alert-success')) {
+    $('#loginModal').modal();
+    $('#closeRegisterForm').css('display', 'none');
+    $('#btnLogin').addClass('link-login');
+    $('#btnRegister').removeClass('link-register');
+  }
 
-    if($('#error').hasClass('alert-danger')) {
-        $('#loginModal').modal();
-        $('#closeRegisterForm').css('display', 'none');
-        $('#btnLogin').addClass('link-login');
-        $('#btnRegister').removeClass('link-register');
-    }
+  if ($('#error').hasClass('alert-danger')) {
+    $('#loginModal').modal();
+    $('#closeRegisterForm').css('display', 'none');
+    $('#btnLogin').addClass('link-login');
+    $('#btnRegister').removeClass('link-register');
+  }
 });
